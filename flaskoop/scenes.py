b8ee2ebe844383726a71d4a107f2f24fb58c89da
Joey Zimmerman <joeyzimmerman17@gmail.com>
2020-03-25 16:25:21 -0400

    Add basic content so some scenes, add ability to traverse scenes.

--- flaskoop/scenes.py
+++ flaskoop/scenes.py
@@ -16,12 +16,46 @@ def death():
     return render_template('scenes/death.html')
 
 
-@bp.route('/tavern')
+@bp.route('/tavern', methods=('GET', 'POST'))
 def tavern():
 
     current_scene = {
-    "title": "The tavern",
-    "intro": "As you walk into the room..."
+    "title": "The Blue Moon Tavern",
+    "intro": """You're in the tavern. Straight ahead there is the bar.
+    To your right there is a strange man, staring at you. Do you:\n
+    1) Go to the bar. 2) Go talk to the strange man."""
+    }
+
+    if request.method == 'POST':
+        choice = request.form['action']
+
+        if choice == "1":
+            return redirect(url_for('scenes.bar'))
+        if choice == "2":
+            return redirect(url_for('scenes.stranger'))
+        else:
+            return redirect(url_for('scenes.death'))
+
+    return render_template('scenes/index.html', scene=current_scene)
+
+
+@bp.route('/bar', methods=('GET', 'POST'))
+def bar():
+
+    current_scene = {
+    "title": "The bar in the tavern",
+    "intro": "You approach the bar in the tavern"
+    }
+
+    return render_template('scenes/index.html', scene=current_scene)
+
+
+@bp.route('/stranger', methods=('GET', 'POST'))
+def stranger():
+
+    current_scene = {
+    "title": "Approaching the stranger",
+    "intro": "You approach the stranger who was eyeing you up."
     }
 
     return render_template('scenes/index.html', scene=current_scene)
@@ -33,6 +67,17 @@ def town_center():
     current_scene = {
     "title": "The town center",
     "intro": "As you wander into the town center..."
+    }
+
+    return render_template('scenes/index.html', scene=current_scene)
+
+
+@bp.route('/trader-wagon')
+def trader_wagon():
+
+    current_scene = {
+    "title": "The trader's wagon on the edge of town",
+    "intro": "You approach the trader's wagon"
     }
 
     return render_template('scenes/index.html', scene=current_scene)
